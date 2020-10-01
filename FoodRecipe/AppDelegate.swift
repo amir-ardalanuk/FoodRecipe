44ea92a0981ce5eb910b1e79b8e9d16d497b1848
Amir <amir@Amirs-MacBook-Pro-2.local>
2020-10-01 14:18:20 +0330

    added pod

--- FoodRecipe/AppDelegate.swift
+++ FoodRecipe/AppDelegate.swift
@@ -11,11 +11,21 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var windows: UIWindow?
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+    
+    private func setupWindow(initailController initVC: UIViewController) {
+        let window  = UIWindow(frame: UIScreen.main.bounds)
+        
+        window.backgroundColor = .black
+        window.rootViewController = UIStoryboard(name:"Main", bundle: nil).instantiateInitialViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
     }
 
 }
